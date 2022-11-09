348eeafec24a845a8f6ed483f8ba6b0c90b59beb
Cypher890 <abdulsuhaib07@gmail.com>
2022-11-09 15:16:14 +0530

    used pretty extension

--- app.js
+++ app.js
@@ -1,117 +1,99 @@
-const express = require('express');
-const { ObjectId } = require('mongodb');
-const { connectToDb, getDb } = require('./db');
+const express = require("express");
+const { ObjectId } = require("mongodb");
+const { connectToDb, getDb } = require("./db");
 
 // init app & middleware
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 // db connection
-let db
+let db;
 
-
-connectToDb((err) =>{
-   if (!err){
-    app.listen(3000, ()=>{
-        console.log('listening on port 3000');
-    })
-    db = getDb()
-   } 
-})
-
-
+connectToDb((err) => {
+  if (!err) {
+    app.listen(3000, () => {
+      console.log("listening on port 3000");
+    });
+    db = getDb();
+  }
+});
 
 // routes myPronest
-app.get('/myPronest', (req, res) => {
+app.get("/myPronest", (req, res) => {
+  // current page
+  //const page = req.query.p || 0
 
-    // current page
-    //const page = req.query.p || 0
-    
-    let myPronest = []
+  let myPronest = [];
 
-
-    db.collection('myPronest')
-    .find()  // cursor toArray forEach 
-    .sort({rank:-1})
-    .forEach(myPrones => myPronest.push(myPrones))
-    .then(()=> {
-        res.status(200).json(myPronest)
+  db.collection("myPronest")
+    .find() // cursor toArray forEach
+    .sort({ rank: -1 })
+    .forEach((myPrones) => myPronest.push(myPrones))
+    .then(() => {
+      res.status(200).json(myPronest);
     })
-    .catch(()=> {
-        res.status(500).json({error: 'Could not find the documents'})
-    })
-})
+    .catch(() => {
+      res.status(500).json({ error: "Could not find the documents" });
+    });
+});
 
-app.get('/myPronest/:id', (req, res) => {
+app.get("/myPronest/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("myPronest")
+      .findOne({ _id: ObjectId(req.params.id) })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could not find the documents" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid doc id" });
+  }
+});
 
-    if (ObjectId.isValid(req.params.id)){
+app.post("/myPronest", (req, res) => {
+  const city = req.body;
 
-        db.collection('myPronest')
-        .findOne({_id: ObjectId(req.params.id)})
-        .then(doc => {
-            res.status(200).json(doc)
-        })
-        .catch(err => {
-            res.status(500).json({error: 'Could not find the documents'})
-        })
-
-    }else {
-        res.status(500).json({error: 'Not a valid doc id'})
-    }
-
-})
-
-app.post('/myPronest',(req ,res)=>{
-    const city = req.body
-
-    db.collection('myPronest')
+  db.collection("myPronest")
     .insertOne(city)
-    .then(result => {
-       res.status(201).json(result)
+    .then((result) => {
+      res.status(201).json(result);
     })
-    .catch(err => {
-        res.status(500).json({err:"could not create new document"})
-    })
-})
+    .catch((err) => {
+      res.status(500).json({ err: "could not create new document" });
+    });
+});
 
-app.delete('/myPronest/:id',(req,res) => {
+app.delete("/myPronest/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("myPronest")
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could not delete the documents" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid doc id" });
+  }
+});
 
-    if (ObjectId.isValid(req.params.id)){
+app.patch("/myPronest/:id", (req, res) => {
+  const updates = req.body;
 
-        db.collection('myPronest')
-        .deleteOne({_id: ObjectId(req.params.id)})
-        .then(doc => {
-            res.status(200).json(doc)
-        })
-        .catch(err => {
-            res.status(500).json({error: 'Could not delete the documents'})
-        })
-
-    }else {
-        res.status(500).json({error: 'Not a valid doc id'})
-    }
-
-
-})
-
-app.patch('/myPronest/:id',(req,res)=>{
-    const updates = req.body
-
-    
-    if (ObjectId.isValid(req.params.id)){
-
-        db.collection('myPronest')
-        .updateOne({_id: ObjectId(req.params.id)},{$set: updates})
-        .then(doc => {
-            res.status(200).json(doc)
-        })
-        .catch(err => {
-            res.status(500).json({error: 'Could not update the documents'})
-        })
-
-    }else {
-        res.status(500).json({error: 'Not a valid doc id'})
-    }
-
-})
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("myPronest")
+      .updateOne({ _id: ObjectId(req.params.id) }, { $set: updates })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could not update the documents" });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid doc id" });
+  }
+});
